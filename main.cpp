fc6feb209ff2a01f40e786beeabe4e9da0e9cd7a
Ahmed Hegazy <ahmedhegazy@Ahmeds-MacBook-Pro.local>
2024-02-24 10:28:02 +0200

    hegazyfucksbedo

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+
+class ahmed
+{
+private:
+    int n;
+public:
+    explicit ahmed(int initial = 0) : n(initial){};
+    void write(){
+        cout << n;
+    };
+};
+
+
+int main() {
+    ahmed m(9);
+
+
+    m.write();
+
+
+    }
+
+
+
